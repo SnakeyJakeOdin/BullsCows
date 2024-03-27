9a01d9667859db5ec374a3c91b15998470ef074f
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-27 16:09:39 +0900

    Create method to check for likeness between two string inputed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        SecretKeeper secretKeeper = new SecretKeeper();
-        System.out.println(Arrays.toString(secretKeeper.getNum()));
+        SecretKeeper secretKeeper = new SecretKeeper("9305");
+        System.out.println(Arrays.toString(secretKeeper.getSecretNum()));
+        System.out.println(Arrays.toString(secretKeeper.checkBullsAndCows("1267")));;
     }
 }
--- src/SecretKeeper.java
+++ src/SecretKeeper.java
@@ -1,14 +1,21 @@
 import java.util.*;
 
 public class SecretKeeper {
-    private final int[] num;
+    private final int[] secretNum;
 
     public SecretKeeper() {
-        this.num = generateNum();
+        this.secretNum = generateNum();
+    }
+    public SecretKeeper(String secret) {
+        int[] arr = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = secret.charAt(i) - 48;
+        }
+        this.secretNum = arr;
     }
 
-    public int[] getNum() {
-        return this.num;
+    public int[] getSecretNum() {
+        return this.secretNum;
     }
 
     private int[] generateNum() {
@@ -26,5 +33,33 @@ public class SecretKeeper {
         }
 
         return secretCode;
+    }
+
+    public int[] checkBullsAndCows(String userGuess) {
+        // Create array of user input
+        int[] guessNum = new int[4];
+        for (int i = 0; i < guessNum.length; i++) {
+            guessNum[i] = userGuess.charAt(i) - 48;
+        }
+        System.out.println(Arrays.toString(guessNum));
+
+        // Compare
+        int numCows = 0;
+        int numBulls = 0;
+        for (int i = 0; i < secretNum.length; i++) {
+            for (int j = 0; j < guessNum.length; j++) {
+                // Cow check
+                if (secretNum[i] == guessNum[j]) {
+                    numCows++;
+                }
+            }
+            // Bull check
+            if (secretNum[i] == guessNum[i]) {
+                numCows--;
+                numBulls++;
+            }
+        }
+
+        return new int[]{numBulls, numCows};
     }
 }
