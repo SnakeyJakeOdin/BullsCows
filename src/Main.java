d1a9b0236dbae7f729cb4604b2db910360c4a217
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-27 15:33:34 +0900

    Generate secret code via a different class

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Create array of unique random numbers
-        Stack<Integer> nums = new Stack<>();
-        for (int i = 0; i <= 9; i++) {
-            nums.add(i);
-        }
-        Collections.shuffle(nums);
-
-        // Generate secret code
-        int[] secretCode = new int[4];
-        for (int i = 0; i < secretCode.length; i++) {
-            secretCode[i] = nums.pop();
-        }
-        System.out.println(Arrays.toString(secretCode));
+        SecretKeeper secretKeeper = new SecretKeeper();
+        System.out.println(Arrays.toString(secretKeeper.getNum()));
     }
 }
