a1270172823edf73947fe9bf8b39e19fbbada999
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-27 15:12:32 +0900

    Create sample of what game will look like

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        String s = """
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.""";
+        System.out.println(s);
     }
 }
