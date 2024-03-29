05d14c6603ec82d5442a0eb29f923616d4f35d46
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-29 15:19:49 +0900

    Start project over for clarity

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Class setup
-        Scanner scanner = new Scanner(System.in);
-        Secret secretKeeper = new Secret();
-        int[] bullsAndCows = secretKeeper.getBullsAndCows(scanner.nextLine());
+        int num = generateSecretNum(4);
+        System.out.println(num);
 
-        // Variables to compare
-        int bulls = bullsAndCows[0];
-        int cows = bullsAndCows[1];
+    }
 
-        // Logic
-        if (bulls > 0 && cows > 0) {
-            System.out.printf("Grade: %d bull(s) and %d cows(s).", bulls, cows);
-        }
-        else if (bulls > 0) {
-            System.out.printf("Grade: %d bull(s).", bulls);
-        }
-        else if (cows > 0) {
-            System.out.printf("Grade: %d cow(s).", cows);
+    public static int generateSecretNum(int len) {
+        if (len > 10) {
+            System.out.printf("Error: can't generate a secret number with a length of %d" +
+                    " because there aren't enough unique digits.", len);
+            return -1;
         }
         else {
-            System.out.print("None.");
+            // random, unique number generation
+            Stack<Integer> stack = new Stack<Integer>();
+            for (int i = 9; i >= 0; i--) {
+                stack.push(i);
+            }
+            while (stack.peek() == 0) {
+                Collections.shuffle(stack);
+            }
+
+            //TODO: Delete after testing
+            System.out.println(stack);
+
+            // unique generation --> secret number
+            int secretNum = 0;
+            for (int i = len - 1; i >= 0; i--) {
+                secretNum += stack.pop() * (int)Math.pow(10, i);
+            }
+            return secretNum;
         }
-        System.out.printf(" The secret code is %s.", "9305");
     }
 }
